0ba6e1cd1242851f2aec05b7b2e4e223865846dc
Yann <yann.eysseric@egs.school>
2022-08-12 19:35:34 +0200

    add files without js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const poneys = [{ name: 'Pinky Pie', strength: '50', speed: '65', charisma: '80' }, { name: 'Rainbow Dash', strength: '80', speed: '75', charisma: '90' }, {
+    name: 'Twilight Sparkle',
+    strength: '90',
+    speed: '45',
+    charisma: '70'
+}, { name: 'Rarity', strength: '30', speed: '50', charisma: '85' }, { name: 'Fluttershy', strength: '67', speed: '64', charisma: '30' }]
