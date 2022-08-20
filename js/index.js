3e10a413dab47fbe1441b50c20bdff499a381daf
Yann <yann.eysseric@egs.school>
2022-08-20 15:17:44 +0200

    modif files

--- js/index.js
+++ js/index.js
@@ -4,29 +4,100 @@ const poneys = [{ name: 'Pinky Pie', strength: '50', speed: '65', charisma: '80'
 { name: 'Rarity', strength: '30', speed: '50', charisma: '85' }, 
 { name: 'Fluttershy', strength: '67', speed: '64', charisma: '30' }];
   
-let image = [];
+// let image = [];
 
 // for (let i = 0; i < 4; i++) {
 //     image[i] = '/img/pony' + (i+2) + '.png';
 // }  
 
-document.body.onload=function() {
-    nombre = 5;
-    p = 0;
-    carrousel = document.getElementById('carrousel');
-    left = document.getElementById('left');
-    right = document.getElementById('right');
+/******** START FONCTIONS *********/
 
-    for (let i = 0; i < nombre; i++) {
-        let div = document.createElement('div');
-        div.classList.add('poney');
-        let img = document.createElement('img');
-        div.append(img);
-        img.src = '/img/pony' + (i+1) + '.png';
-        carrousel.append(div);
-    }   
-}
+function translate() {
+    panneau.style.transform = 'translateX('+ p * 300 +'px)';
+};
 
-left.onclick=function() {
-    p--;
-}
+function masquer() {
+    if (p == 2) {
+        document.getElementById('left').src='./img/arrowDis.png';
+    } else {
+        document.getElementById('left').src='./img/arrow.png';
+    };
+
+    if (p == -2) {
+        document.getElementById('right').src='./img/leftDis.png';
+    } else {
+        document.getElementById('right').src='./img/right.png';
+    };
+};
+
+/******** END FONCTIONS *********/
+
+
+let nombre = 5;
+let p = 0;
+const carrousel = document.getElementById('carrousel');
+const panneau = document.getElementById('panneau');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+
+carrousel.style.width = 300 + 'px';
+
+for (let i = 0; i < nombre; i++) {
+    let div = document.createElement('div');
+    div.classList.add('poney');
+    let img = document.createElement('img');
+    div.append(img);
+    img.src = './img/pony' + (i+1) + '.png';
+    panneau.append(div);
+};
+
+left.addEventListener('click', function() {
+    if(p < 2) {
+        p++;
+    };
+    translate();
+    masquer();
+});
+
+right.addEventListener('click', function() {
+    if(p > -2) {
+        p--;
+    };
+    translate();
+    masquer();
+});
+
+// let poney = document.querySelectorAll('.poney div');
+// poney.forEach(poney => {
+//     div.addEventListener('mouseover', () => {
+//         div.classList.add('hover');
+//     });
+// });
+
+// console.log(poney);
+
+// let poney = document.querySelectorAll('.poney');
+// poney.addEventListener('mouseover',() => {
+//     div.style.backgroundColor = 'purple';
+// });
+
+let poney = document.querySelectorAll('.poney');
+
+poney.forEach(poney => {
+    let div2 = document.createElement('div');
+    div2.classList.add('hover');
+    poney.append(div2);
+    let hover = document.querySelector('.hover');
+    let h3 = document.createElement('h3');
+    h3.innerText = poneys.name;
+    hover.append(h3);
+});
+
+
+// let poney = document.querySelectorAll('.poney');
+
+// for (let i = 0; i < nombre; i++) {
+//     let div2 = document.createElement('div');
+//     div2.classList.add('.hover');
+//     poney.append(div2);
+// }
