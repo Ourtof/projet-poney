5773dc7dea2b1f3af9d702673ef182b8f3b4dc50
Yann <yann.eysseric@egs.school>
2022-08-22 18:03:16 +0200

    modif files js

--- js/index.js
+++ js/index.js
@@ -3,17 +3,11 @@ const poneys = [{ name: 'Pinky Pie', strength: '50', speed: '65', charisma: '80'
 { name: 'Twilight Sparkle', strength: '90', speed: '45', charisma: '70'}, 
 { name: 'Rarity', strength: '30', speed: '50', charisma: '85' }, 
 { name: 'Fluttershy', strength: '67', speed: '64', charisma: '30' }];
-  
-// let image = [];
-
-// for (let i = 0; i < 4; i++) {
-//     image[i] = '/img/pony' + (i+2) + '.png';
-// }  
 
 /******** START FONCTIONS *********/
 
 function translate() {
-    panneau.style.transform = 'translateX('+ p * 300 +'px)';
+    panneau.style.transform = 'translateX('+ y * 300 +'px)';
 };
 
 function masquer() {
@@ -33,8 +27,7 @@ function masquer() {
 /******** END FONCTIONS *********/
 
 
-let nombre = 5;
-let p = 0;
+let y = 0;
 const carrousel = document.getElementById('carrousel');
 const panneau = document.getElementById('panneau');
 const left = document.getElementById('left');
@@ -42,26 +35,55 @@ const right = document.getElementById('right');
 
 carrousel.style.width = 300 + 'px';
 
-for (let i = 0; i < nombre; i++) {
+let poney = document.querySelectorAll('.poney');
+let jauge = document.querySelectorAll('.jauge');
+const resume = ["I'm Pinkie Pie", "I'm Rainbow Dash", "I'm Twilight Sparkle", "I'm Rarity", "I'm Fluttershy"];
+
+for (let i = 0; i < poneys.length; i++) {
     let div = document.createElement('div');
     div.classList.add('poney');
     let img = document.createElement('img');
     div.append(img);
     img.src = './img/pony' + (i+1) + '.png';
     panneau.append(div);
+
+
+    // Partie Hover
+    let div2 = document.createElement('div');
+    div2.classList.add('hover');
+    div.append(div2);
+    h3 = document.createElement('h3');
+    h3.innerText = poneys[i].name;
+    div2.prepend(h3);
+    let p = document.createElement('p');
+    p.innerText = resume[i];
+    div2.append(p);
+    let div3 = document.createElement('div');
+    div3.classList.add('jauge');
+    div.append(div3);
+    console.log(div3);
 };
 
+
+
+// poneys.forEach(poney => {
+//     let div2 = document.createElement('div');
+//     div2.classList.add('hover');
+//     poneys.append(div2);
+// });
+
+
 left.addEventListener('click', function() {
-    if(p < 2) {
-        p++;
+    if(y < 2) {
+        y++;
     };
     translate();
     masquer();
 });
 
 right.addEventListener('click', function() {
-    if(p > -2) {
-        p--;
+    if(y > -2) {
+        y--;
     };
     translate();
     masquer();
@@ -74,24 +96,23 @@ right.addEventListener('click', function() {
 //     });
 // });
 
-// console.log(poney);
 
 // let poney = document.querySelectorAll('.poney');
 // poney.addEventListener('mouseover',() => {
 //     div.style.backgroundColor = 'purple';
 // });
 
-let poney = document.querySelectorAll('.poney');
 
-poney.forEach(poney => {
-    let div2 = document.createElement('div');
-    div2.classList.add('hover');
-    poney.append(div2);
-    let hover = document.querySelector('.hover');
-    let h3 = document.createElement('h3');
-    h3.innerText = poneys.name;
-    hover.append(h3);
-});
+
+// poney.forEach(poney => {
+//     let div2 = document.createElement('div');
+//     div2.classList.add('hover');
+//     poney.append(div2);
+//     let hover = document.querySelector('.hover');
+//     let h3 = document.createElement('h3');
+//     h3.innerText = poneys.name;
+//     hover.append(h3);
+// });
 
 
 // let poney = document.querySelectorAll('.poney');
